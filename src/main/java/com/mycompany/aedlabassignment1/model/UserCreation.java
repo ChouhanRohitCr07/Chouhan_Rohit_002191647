f7863035bf8c2d805bc97840dca40d637eaec13f
Chouhan Rohit <chouhan.ro@northeastern.edu>
2021-09-26 17:47:29 -0400

    Model and UI packages are created, also four classes are created into the packages

---
+++ src/main/java/com/mycompany/aedlabassignment1/model/UserCreation.java
@@ -0,0 +1,183 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aedlabassignment1.model;
+
+/**
+ *
+ * @author jarvis
+ */
+public class UserCreation {
+    
+    private String name;
+    private String address;
+    private String dateOfBirth;
+    private long telephone;
+    private String faxNumber;
+    private String emailAddress;
+    private String socialSecurityNumber;
+    private String medicalRecordNumber;
+    private String healthPlanBeneficiaryNumber;
+    private String bankAccountNumber;
+    private String licenseNumber;
+    private String vehicleIndentificationSerialNumber;
+    private String deviceIdentificationSerialNumber;
+    private String linkedin;
+    private String IPAddress;
+    private String uniqueIdentificationNumber;
+    private String ImageSrc;
+    private String FingerPrintImageSrc;
+    
+     public String getFingerPrintImageSrc() {
+        return FingerPrintImageSrc;
+    }
+
+    public void setFingerPrintImageSrc(String FingerPrintImageSrc) {
+        this.FingerPrintImageSrc = FingerPrintImageSrc;
+    }
+
+    public String getImageSrc() {
+        return ImageSrc;
+    }
+
+    public void setImageSrc(String ImageSrc) {
+        this.ImageSrc = ImageSrc;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(long telephone) {
+        this.telephone = telephone;
+    }
+
+    
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getMedicalRecordNumber() {
+        return medicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String medicalRecordNumber) {
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+
+    public String getHealthPlanBeneficiaryNumber() {
+        return healthPlanBeneficiaryNumber;
+    }
+
+    public void setHealthPlanBeneficiaryNumber(String healthPlanBeneficiaryNumber) {
+        this.healthPlanBeneficiaryNumber = healthPlanBeneficiaryNumber;
+    }
+
+    public String getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getVehicleIndentificationSerialNumber() {
+        return vehicleIndentificationSerialNumber;
+    }
+
+    public void setVehicleIndentificationSerialNumber(String vehicleIndentificationSerialNumber) {
+        this.vehicleIndentificationSerialNumber = vehicleIndentificationSerialNumber;
+    }
+
+    public String getDeviceIdentificationSerialNumber() {
+        return deviceIdentificationSerialNumber;
+    }
+
+    public void setDeviceIdentificationSerialNumber(String deviceIdentificationSerialNumber) {
+        this.deviceIdentificationSerialNumber = deviceIdentificationSerialNumber;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIPAddress() {
+        return IPAddress;
+    }
+
+    public void setIPAddress(String IPAddress) {
+        this.IPAddress = IPAddress;
+    }
+
+    public String getUniqueIdentificationNumber() {
+        return uniqueIdentificationNumber;
+    }
+
+    public void setUniqueIdentificationNumber(String uniqueIdentificationNumber) {
+        this.uniqueIdentificationNumber = uniqueIdentificationNumber;
+    }
+
+    
+    
+    
+  
+}
